c8da4035f3a82fb6ad7343d4250df93c384d5ae6
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-01-06 20:06:10 -0800

    figuring out structure of to do list

--- src/index.js
+++ src/index.js
@@ -3,36 +3,8 @@ import './style.css';
 import test from './print.js';
 
 
+const btn = document.querySelector('.addBtn')
 
-const header = document.createElement('h1')
-const input = document.createElement('input')
-const button = document.createElement('button')
-
-
-
-
-
-header.textContent = 'To Do'
-button.textContent = 'Add To Do'
-
-header.classList.add('header')
-input.classList.add('myInput')
-button.classList.add('myBtn')
-
-
-
-document.body.appendChild(header)
-document.body.appendChild(input)
-document.body.appendChild(button)
-
-
-
-button.addEventListener('click', () => {
-   const myToDo = document.createElement('p')
-   myToDo.textContent = input.value
-   document.body.appendChild(myToDo)
-   input.value = ''
-
+btn.addEventListener('click', () => {
+    alert('test, it works!')
 })
-
-test()
