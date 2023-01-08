bc05c4488833eb69946db20123f65fd825e1ff6d
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-01-07 20:15:12 -0800

    figure out how to dynamically add todos with objects

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import _, { head } from 'lodash';
+import _, { add, head } from 'lodash';
 import './style.css';
 import test from './print.js';
 
 
-const btn = document.querySelector('.addBtn')
+const btn = document.createElement('button')
+const input = document.createElement('input')
+const todoContainer = document.querySelector('.toDoContainer')
+const main = document.querySelector('.main')
+
+const addToDo = document.querySelector('.addToDo')
+
+input.classList.add('myInput')
+btn.classList.add('addBtn')
+btn.textContent = 'Add'
+
+addToDo.addEventListener('click', () => {
+    todoContainer.innerHTML = ''
+    
+    todoContainer.appendChild(input)
+    todoContainer.appendChild(btn)
+})
 
 btn.addEventListener('click', () => {
-    alert('test, it works!')
+    const todo = document.createElement('p')
+    todo.textContent = input.value
+    main.appendChild(todo);
+    input.value = ''
 })
